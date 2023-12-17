a684f425f73a8187372b08e4ec5910b81496739e
YouYou94 <ybh4115@gmail.com>
2023-12-17 18:00:35 +0900

    Refactor

--- ALGORITHM/프로그래머스/lv.2/lv.2 k진수에서 소수 개수 구하기.js
+++ ALGORITHM/프로그래머스/lv.2/lv.2 k진수에서 소수 개수 구하기.js
@@ -1,12 +1,9 @@
 function isPrimeNumber(number) {
-  if (number <= 1) {
-    return false;
-  }
-  for (let i = 2; i <= Math.sqrt(number); i++) {
-    if (number % i === 0) {
-      return false;
-    }
-  }
+  if (number <= 1) return false;
+
+  for (let i = 2; i <= Math.sqrt(number); i++)
+    if (number % i === 0) return false;
+
   return true;
 }
 
@@ -16,9 +13,7 @@ function solution(n, k) {
   let numArr = num.split("0"); // 변환한 숫자를 0을 기준으로 나눠 배열에 저장
 
   for (let i = 0; i < numArr.length; i++) {
-    if (isPrimeNumber(Number(numArr[i]))) {
-      answer++; // 해당 숫자가 소수면 answer++
-    }
+    if (isPrimeNumber(Number(numArr[i]))) answer++; // 해당 숫자가 소수면 answer++
   }
 
   return answer;
